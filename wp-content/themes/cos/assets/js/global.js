eab993aa7079a8bb9c29a2a17b12b9dd4ad90ee3
Andy <aknapp1@gmail.com>
2019-03-04 05:19:29 -0500

    link active section

--- wp-content/themes/cos/assets/js/global.js
+++ wp-content/themes/cos/assets/js/global.js
@@ -2,11 +2,13 @@ var body = document.body;
 var html = document.documentElement;
 var vw = window.innerWidth;
 var url = window.location;
+var path = url.pathname;
 var toggle = document.querySelector('.menu-toggle');
 var menu = document.querySelector('#site-navigation');
 var sections = document.querySelectorAll('.major-section');
 var lists = document.querySelectorAll('.sale-list');
 var more = document.querySelector('.view-more');
+var links = document.querySelectorAll('.menu-item a');
 
 // navigation
 toggle.addEventListener('click', function (event) {
@@ -39,14 +41,12 @@ function singleListToggle() {
 singleListToggle();
 
 
-
-
 // smooth scroll, update url with current section
-document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    var curAnchor = this.getAttribute('href').split('#')[1];
-    history.pushState(null, null, curAnchor);
+    //var curAnchor = this.getAttribute('href').split('#')[1];
+	//history.pushState(null, '', '/' + curAnchor);
 
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth',
@@ -56,22 +56,56 @@ document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
 });
 
 
-// window.onscroll = function() {
-//   // Don't run the rest of the code if every section is already visible
-//   if (!document.querySelectorAll('.major-section:not(.current)')) return;
+// highlight nav item for current section
+window.onscroll = function() {
+	var fromTop = window.scrollY;
+
+  for(var i = 0; i < sections.length; i++) {
+
+    if (sections[i].getBoundingClientRect().top <= window.innerHeight / 3) {
+
+		var currentSection = sections[i].getAttribute('id');
+
+		sections[i].classList.add('current');
+		//curLink.classList.add('active');
+
+ // THROTTLE THIS SHIT
+
+		links.forEach(function(link) {
+			var hash = link.getAttribute('href');
+			var active = hash.replace('#', '');
+
+			console.log('active ' + active);
+			console.log('current ' + currentSection);
+
+			if(active == currentSection) {
+				link.classList.add('active');
+			} else {
+				link.classList.remove('active');
+			}
+		});
+
+    } else {
+        sections[i].classList.remove('current');
+    }
+
+  }
+
+};
+
+// window.addEventListener("scroll", event => {
+//   let fromTop = window.scrollY;
 //
-//   // Run this code for every section in sections
-//   for (const section of sections) {
-//     if (section.getBoundingClientRect().top <= window.innerHeight * 0.75 && section.getBoundingClientRect().top > 0) {
-//       section.classList.add('current');
-//       var currentSection = section.getAttribute('id');
+//   links.forEach(link => {
+//     let section = document.querySelector(link.hash);
 //
-//       // var curAnchor = this.getAttribute('href').split('#')[1];
-//
-//       history.pushState(null, '', currentSection);
-//       console.log(currentSection);
+//     if (
+//       section.offsetTop <= fromTop &&
+//       section.offsetTop + section.offsetHeight > fromTop
+//     ) {
+//       link.classList.add("current");
 //     } else {
-//         section.classList.remove('current');
+//       link.classList.remove("current");
 //     }
-//   }
-// };
+//   });
+// });
