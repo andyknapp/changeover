45366e4fbe08731c28b0873713b4decdeb4f654b
Andy <aknapp1@gmail.com>
2020-01-20 09:31:45 -0500

    new template

---
+++ wp-content/themes/cos/page-home.php
@@ -0,0 +1,305 @@
+<?php
+// Template Name: Home Page
+
+get_header();
+?>
+
+	<section id="intro" class="intro major-section">
+		<div class="container-mid">
+			<header>
+				<h1 class="heading-2 underline">Estate & </br>Tag Sales</h1>
+			</header>
+			<div class="intro-content">
+				<p>Since 1977, we have provided complete estate sale services and professionaly conducted in-home Tag Sales in Delaware, Pennsylvania and New Jersey.</p>
+				<p>Whether you are a private individual, executor, trust officer, real estate agent or attorney, <span>Changeover Sales</span> will be be happy to consult with you to best serve your needs in the disposition of one item or a houseful.</p>
+			</div>
+		</div>
+	</section>
+
+	<hr class="spacer">
+
+	<section id="next-sale" class="sale major-section sticky-parent">
+		<header class="sale-header">
+			<?php // if there is an active sale ?>
+				<h2 class="heading-1">Next Sale</h2>
+
+			<?php //else : ?>
+				<!-- <h2 class="heading-2">Upcoming Sale</h2> -->
+				<ul class="address">
+                    <li>305 N Ashview Lane</li>
+					<li>Greenville, DE 19807</li>
+                    <li class="break">Applecross (off Montchanin Rd/Rte 100)</li>
+				</ul>
+
+				<ul class="date-time">
+                    <li>Wednesday Jan 8 <span>6pm - 8pm*</li>
+					<li>Thursday Jan 9 <span>10am - 4pm</span></li>
+					<li>Friday Jan 10 <span>10am - 4pm</span></li>
+					<li>Saturday Jan 11 <span>9am - 1pm</span></li>
+				</ul>
+
+				<div class="sale-meta">
+					<a href="https://www.google.com/maps/dir//305+N+Ashview+Ln,+Wilmington,+DE+19807/@39.7839198,-75.5942162,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x89c6fc4674c1705d:0x1eb7ae6c256a7a24!2m2!1d-75.5920275!2d39.7839198!3e0" class="external-link" target="_blank">
+						<svg class="direction">
+							<use xlink:href="#icon-location"></use>
+						</svg>
+						<span>Get Directions</span>
+					</a>
+
+					<a href="https://www.estatesales.net/DE/Wilmington/19807/2444940" class="external-link" target="_blank">
+						<svg class="photos">
+							<use xlink:href="#icon-photos"></use>
+						</svg>
+						<span>View Photos</span>
+					</a>
+
+					<a href="https://www.facebook.com/ChangeoverSales" class="external-link" target="_blank">
+						<svg class="fb">
+							<use xlink:href="#icon-facebook"></use>
+						</svg>
+						<span>Visit us on Facebook</span>
+					</a>
+				</div>
+		</header>
+
+		<div class="sale-description container-small">
+			<p class="copy-1">High end furnishings, art, rugs, sterling, jewelry, accessories, etc. All in excellent condition! <span>*Line numbers start at 4pm on Wednesday</span></p>
+		</div>
+
+
+
+		<div class="sale-lists sale-cols container-site">
+
+			<div class="sale-col">
+				<div class="sale-list  sale-list-a">
+					<header class="list-header">
+						<h3 class="heading-4">Furniture</h3>
+						<svg>
+							<use xlink:href="#icon-down"></use>
+						</svg>
+					</header>
+					<ul class="list">
+                        <li>Mahogany HICKORY CHAIR dining table & chairs</li>
+                        <li>Maitland Smith center table</li>
+                        <li>Sectional sofa</li>
+                        <li>Drexel Heritage matching upholstered arm chairs</li>
+                        <li>Maitland Smith writing desk</li>
+                        <li>Matching love seats by Ferguson Copeland, Ltd.</li>
+                        <li>Pair slipper chairs by Taylor King</li>
+                        <li>Coffee table by Chelsea House</li>
+                        <li>Matching upholstered chairs by Ardley Hall</li>
+                        <li>Four matching leather wing chairs by Maitland Smith</li>
+                        <li>Grand Maitland Smith rotating coffee table</li>
+                        <li>Drexel Heritage serving chest</li>
+                        <li>Cherry farmhouse table w/6 chairs</li>
+                        <li>King bed, upholstered headboard</li>
+                        <li>Queen bed w/side cabinets</li>
+                        <li>Chinoiserrie secretary desk, excellent!</li>
+                        <li>Hekman executive desk</li>
+                        <li>Painted stacking chest, Eddy West</li>
+                        <li>Leather office chair</li>
+                        <li>Counter stools, upholstered, 4 available</li>
+                        <li>Barstools, swivel seats, 5 available</li>
+                        <li>Maitland Smith Regency style settee</li>
+                        <li>Upholstered benches</li>
+                        <li>Chaise lounge</li>
+                        <li>Storage chests, bookcases</li>
+                        <li class="more">More!</li>
+					</ul>
+				</div>
+
+				<div class="sale-list sale-list-d">
+                    <header class="list-header">
+						<h3 class="heading-4">Also</h3>
+						<svg>
+							<use xlink:href="#icon-down"></use>
+						</svg>
+					</header>
+					<ul class="list">
+                        <li>EFX Eliptical</li>
+                        <li>Outdoor furniture, planters</li>
+                        <li>Large cast iron urns/planters</li>
+                        <li>Outdoor heater</li>
+                        <li>Stereo components - Kenwood, Yamaha, Sony, Harmon Karden, Energy, Rotel, Integra, Panamax, MORE!</li>
+                        <li>Model cars, 1/18, Exact Detail, Ertl, Sunstar</li>
+                        <li>Hess trucks (some new in boxes)</li>
+                        <li>TVs - Pioneer Elite, etc. big & small</li>
+                        <li>Tools, bikes, roller blades, more!</li>
+                        <li class="more">More!</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="sale-col">
+				<div class="sale-list sale-list-b">
+                    <header class="list-header">
+						<h3 class="heading-4">Art</h3>
+						<svg>
+							<use xlink:href="#icon-down"></use>
+						</svg>
+					</header>
+					<ul class="list">
+						<li>Autographed movie posters</li>
+                        <li>Many framed & unframed prints</li>
+                        <li class="more">More!</li>
+					</ul>
+				</div>
+
+				<div class="sale-list sale-list-e">
+                    <header class="list-header">
+						<h3 class="heading-4">Silver & Jewelry</h3>
+						<svg>
+							<use xlink:href="#icon-down"></use>
+						</svg>
+					</header>
+					<ul class="list">
+                        <li>Fine 14k & 18k gold necklaces, bracelets, earrings, rings</li>
+                        <li>Phillip Stein diamond watch</li>
+                        <li>18k Gold Tag Heuer watch</li>
+                        <li>Tiffany jewelry</li>
+                        <li>Lizzy Fortunato earrings</li>
+                        <li>Sterling jewelry</li>
+                        <li>Pandora bracelets</li>
+                        <li>Antique sterling & enamel Russian purse..so unique!</li>
+                        <li>"Royal Danish" Sterling tea/coffee service</li>
+                        <li>Royal Crest Sterling "Wildflower" flatware set - 190 pcs.!!</li>
+                        <li>Kirk Sterling vermeil "Repousse" flatware - various pieces </li>
+                        <li>Kirk Sterling "Repousse" bowl, dishes</li>
+                        <li>Sterling bowls, dishes, trays, etc.</li>
+                        <li>Costume jewelry</li>
+                        <li class="more">More!</li>
+					</ul>
+
+				</div>
+			</div>
+
+			<div class="sale-col">
+				<div class="sale-list sale-list-c">
+                    <header class="list-header">
+						<h3 class="heading-4">Accessories</h3>
+						<svg>
+							<use xlink:href="#icon-down"></use>
+						</svg>
+					</header>
+                    <ul class="list">
+                        <li>Oriental rugs & runners...BEAUTIFUL!! Many sizes!</li>
+                        <li>Gilded mirrored sconces</li>
+                        <li>Fabulous floral arrangements, NDI, many!</li>
+                        <li>Faux trees & topiariesv
+                        <li>Excellent lamps & floor lamps</li>
+                        <li>Large candle holders, urns, hurricanes, etc.</li>
+                        <li>Beautiful mirrors</li>
+                        <li>Excellent custom decorator pillows</li>
+                        <li>Fireplace screens</li>
+                        <li>Chelsea House urns, vases</li>
+                        <li>Portmeirion dishes/serving pieces</li>
+                        <li>Longaberger baskets...a collection!</li>
+                        <li>Henckels knives</li>
+                        <li>Cutco cookware</li>
+                        <li>Mauviel Copper stock pot</li>
+                        <li class="more">More!</li>
+                    </ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="sale-close container-small">
+			<ul>
+				<li>So much more...still unearthing! Please, cash or check only, or credit card with fees (2.75%)</li>
+				<li>Follow the <span class="pink">pink signs</span> & <i>please</i> obey signage.</li>
+				<li>As always, <i>please</i> respect the home owner's and neighbors' properties and park thoughtfully. Thanks!</li>
+			</ul>
+		</div>
+
+		<aside class="upcoming container-small">
+			<header class="upcoming-header flex animation-element fade-up" data-trigger="0.925">
+				<h4 class="heading-3 underline">Upcoming Sales</h4>
+				<p>Check back soon for more information!</p>
+			</header>
+			<div class="upcoming-sales animation-element fade" data-trigger="0.91">
+				<ul class="address">
+					<li>January 22 - 25</li>
+					<li>Edenridge</li>
+					<li>Wilmington, DE</li>
+				</ul>
+				<ul class="address">
+					<li>January 31 & Feb 1</li>
+					<li>Guilty Girls Warehouse Sale</li>
+					<li>DuPont CC</li>
+				</ul>
+                <ul class="address">
+					<li>TBA</li>
+					<li>Centreville Reserve</li>
+					<li>Centreville, DE</li>
+				</ul>
+			</div>
+		</aside>
+	</section>
+
+	<hr class="spacer">
+
+	<section id="how-it-works" class="how-it-works major-section">
+		<div class="container-mid animation-element fade-up" data-trigger="0.9">
+			<h5 class="heading-1">How it Works</h5>
+			<span>We take care of everything</span>
+			<p>All our client needs to do is take what they want and leave the rest to us. We will sort saleable from non-saleable, arrange, price, tag, advertise and sell everything in 2 or 3 days leaving your home “broom clean” ready for the cleaning crew (we can arrange for that, too!).</p>
+			<p>Within a week, we will furnish you with a check for the proceeds and a report with an itemized accounting of everything that sold.</p>
+			<p>Our Tag Sales are professionally conducted with the utmost respect for your home & possessions. Attention to even the smallest detail, strong security measures and our friendly professional staff guarantees excellent results.</p>
+			<p>Our over 4 decades of experience with antiques, fine art, jewelry and household items and our constant exposure to the ever-changing market ensures that your possessions will be properly priced to produce maximum revenue for our client.</p>
+			<p>We also offer a full & partial estate purchases. If a Tag Sale isn't possible, we can offer a buyout of complete or partial contents of a home. After a purchase price has been agreed to, we will arrange for the packing & removal of all items and pay you immediately.</p>
+		</div>
+	</section>
+
+	<section id="testimonials" class="testimonials major-section">
+		<div class="container-mid animation-element fade-up" data-trigger="0.9">
+			<ul class="slider">
+			<li class="slide">
+				<div class="container-slide">
+					<blockquote>
+						<p><span class="open">&ldquo;</span>Thank you so much for organizing and running the Tag Sale at my Aunt’s house. I was not very comfortable doing a sale...but you two put me immediately at ease. Thank you for treating her things with such respect.<span class="close">&rdquo;</span></p>
+					</blockquote>
+					<cite>Susie Racobaldo </br>Kennett Square, PA</cite>
+				</div>
+
+			</li>
+
+			<li class="slide">
+				<div class="container-slide">
+					<blockquote>
+						<p><span class="open">&ldquo;</span>...Kay Reilly and the team at Changeover Sales made a very difficult time much easier and as painless as possible.... What could have been a very time consuming process...was handled professionally and with the utmost consideration and courtesy by the team at Changeover Sales. Their experience and expertise was greatly appreciated and an invaluable service.<span class="close">&rdquo;</span</p>
+					</blockquote>
+				</div>
+			</li>
+		</ul>
+		</div>
+	</section>
+
+	<section id="about-us" class="about major-section">
+		<div class="container-mid animation-element fade-up" data-trigger="0.9">
+			<h5 class="heading-1">About Us</h5>
+			<!-- <img src="https://www.fillmurray.com/400/500" alt=""  /> -->
+			<p>Founded by Kay Reilly in 1977, we continue to be family owned and operated with a combined 70+ years of tag sales and appraisal experience. Word of mouth and customer referrals are the backbone of our thriving business and sterling reputation.</p>
+			<p>We are very proud of our friendly, knowledgeable, professional staff, many who have been with us from the start! We also maintain a vast number of experts who contribute valuable information and opinions on art, antiques, jewelry, etc.</p>
+		</div>
+	</section>
+
+	<hr class="spacer">
+
+	<section id="contact" class="contact major-section">
+		<div class="container-mid animation-element fade-up" data-trigger="0.9">
+			<h5 class="heading-1">Contact</h5>
+			<div class="contact-container">
+				<aside class="contact-info">
+					<span class="copy-1">Kay Reilly</span>
+					<span class="copy-1">Patty Souffie</span>
+					<a href="mailto:changeoversales@aol.com" target="_blank">changeoversales@aol.com</a>
+					<a href="tel:1-302-562-5385">302-562-5385</a>
+				</aside>
+
+				<?php gravity_form( 1, false, false, false, '', false ); ?>
+			</div>
+		</div>
+	</section>
+
+<?php
+get_footer();
