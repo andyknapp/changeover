d82f04192c1cdc1b5e22049a837c22c423bbb990
Andy <aknapp1@gmail.com>
2020-04-24 17:18:06 -0400

    shop updates

--- wp-content/themes/cos/functions.php
+++ wp-content/themes/cos/functions.php
@@ -246,6 +246,9 @@ function cos_woocommerce_template_loop_product_title() {
 add_filter( 'woocommerce_shop_loop_item_title', 'cos_woocommerce_template_loop_product_title' );
 
 
+// make category conditional on single prod page
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
 
 // Add the opening div to the img
 function add_img_wrapper_start() {
@@ -258,3 +261,5 @@ function add_img_wrapper_close() {
     echo '</div>';
 }
 add_action( 'woocommerce_before_shop_loop_item_title', 'add_img_wrapper_close', 12, 2 );
+
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
