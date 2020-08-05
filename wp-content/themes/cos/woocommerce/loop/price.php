c439c39058928a3a2f388fa7733d88b2bb29aa86
Andy <aknapp1@gmail.com>
2020-08-05 09:34:45 -0400

    reveal prices for sale

--- wp-content/themes/cos/woocommerce/loop/price.php
+++ wp-content/themes/cos/woocommerce/loop/price.php
@@ -23,7 +23,7 @@ global $product;
 ?>
 
 <?php if ( $price_html = $product->get_price_html() ) : ?>
-	<span class="price"><?php //echo $price_html; ?></span></div>
+	<span class="price"><?php echo $price_html; ?></span></div>
 <?php else : ?>
     </div>
 <?php endif; ?>
